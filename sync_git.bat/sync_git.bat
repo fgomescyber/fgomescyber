19c4a7ad9fb637733f3019ccc1bbad07729c530b
fgomescyber <fgomescyber@gmail.com>
2025-05-12 20:01:33 +0000

    Sync autom├ítico em 12/05/2025 20:01:33,55

---
+++ sync_git.bat/sync_git.bat
@@ -0,0 +1,16 @@
+@echo off
+cd /d "C:\Users\Saints Nandy\Documents\Project_Git_Github\fgomescyber"
+
+echo =============================
+echo [1/3] Puxando mudanças do GitHub
+git pull origin main
+
+echo [2/3] Adicionando arquivos novos e alterados
+git add .
+
+echo [3/3] Commitando e enviando para o GitHub
+git commit -m "Sync automático em %date% %time%"
+git push origin main
+
+echo ✅ Sincronização concluída.
+pause
